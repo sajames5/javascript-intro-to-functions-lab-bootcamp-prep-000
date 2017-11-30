adc79984dee3f3293f8bb05fa1ff1c083a27b06f
Stephanie <s.a.james5@gmail.com>
2017-11-30 17:50:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,8 +9,5 @@ function sayHiToGrandma (string) {
   if (string === "I love you, Grandma.")
     return ("I love you, too.") 
 
-  if (string === "I love you, Grandma")
-    return ("I love you, too") 
-
   if (string === 'HELLO') 
     return ("YES INDEED!")}
