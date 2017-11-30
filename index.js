550a326c27f533c5c91b4d382039edd321157b02
Stephanie <s.a.james5@gmail.com>
2017-11-30 17:49:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ function logWhisper(string) {console.log(string.toLowerCase('hello'))}
 function sayHiToGrandma (string) {
   if (string === 'hello')
     return ("I can't hear you!")
+<<<<<<< HEAD
   if (string === "I love you, Grandma.")
     return ("I love you, too.") 
+=======
+  if (string === "I love you, Grandma")
+    return ("I love you, too") 
+>>>>>>> 3681677c700021122f81377c748e360af9686b4b
   if (string === 'HELLO') 
     return ("YES INDEED!")}
